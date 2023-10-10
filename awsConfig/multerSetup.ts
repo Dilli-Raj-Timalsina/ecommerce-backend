93c5c7edf3c520c9a4a23fb1941829114d5fdb06
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-10-10 08:08:05 +0545

    aws conf file added

---
+++ awsConfig/multerSetup.ts
@@ -0,0 +1,36 @@
+// Multer is a node.js middleware for handling multipart/form-data
+import multer from "multer";
+
+// /*
+// setup for Disk storage/to store on your current pc/machine
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, "./uploads");
+//     },
+
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + "-" + file.originalname);
+//     },
+// });
+
+// const fileFilter = (req, file, cb) => {
+//     if (file.mimetype.split("/")[0] === "image") {
+//         cb(null, true);
+//     } else {
+//         cb(new AppError("Only Image can be inserted"), false);
+//     }
+// };
+// const upload = multer({ storage });
+// // */
+
+// setup for cloud storage / to create a buffer
+const storage = multer.memoryStorage();
+
+const upload = multer({
+    storage: storage,
+    // fileFilter,
+    // limits: { fileSize: 1000000000 },
+});
+
+module.exports = upload;
